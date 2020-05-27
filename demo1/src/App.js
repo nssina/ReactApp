a2729c2d9c4c71fc3aba5e7f042460a473f19101
nssina <rabie_sina@yahoo.com>
2020-05-27 09:36:40 -0400

    Added dynamic content

--- demo1/src/App.js
+++ demo1/src/App.js
@@ -1,26 +1,35 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { directive } from '@babel/types';
 
-//Adding Dynaimc Data to our App
+//Adding Dynaimc Data to our react app
 
 export default class App extends Component {
-  render() {
 
-    constructor(props)
+  constructor(props)
     {
       super(props);
       this.state = {
-        userName : "Sina"
+        userName : "Sina",
+        course : "iOS/Web Developer"
       }
     }
-    return(
+
+    changeStateData =() => {
+      this.setState(
+        { userName: this.state.userName == "Sina" ? "Alex" : "Sina" }
+      )
+    }
+
+
+  render =() =>
     <div>
       <h4 className="bg-primary text-white text-center p-2">
-        { this.state.userName } iOS/React Develoer
+        { this.state.userName } Course
+        <button className="btn btn-danger m-2"
+                onClick={ this.changeStateData }>
+                  Change the State
+        </button>
       </h4>
     </div>
-    )
-  };
-}
+  }
