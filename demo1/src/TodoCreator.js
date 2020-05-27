2f2aceca720d14d1854f068f3ae741aa6053d318
nssina <rabie_sina@yahoo.com>
2020-05-27 11:28:05 -0400

    Create TodoCreator

---
+++ demo1/src/TodoCreator.js
@@ -0,0 +1,11 @@
+// 3rd Child Component
+// Do only one Feature
+
+import React, {Component} from 'react'
+
+export class TodoCreator extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {newItemText : " "};
+    }
+};
