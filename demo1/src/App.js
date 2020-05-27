9db550188c568fa07303aad907bb7b4fe32e249d
nssina <rabie_sina@yahoo.com>
2020-05-27 10:22:36 -0400

    Added todo task

--- demo1/src/App.js
+++ demo1/src/App.js
@@ -11,8 +11,31 @@ export default class App extends Component {
       super(props);
       this.state = {
         userName : "Sina",
-        course : "iOS/Web Developer"
+        todoItems : [
+          { action : "Buy a flower", done: false},
+          { action : "Do Workout", done: true},
+          { action : "Study Programming", done: false},
+          { action : "My friend", done: true}
+        ],
+        newItemText : " "
       }
+    }
+
+    updateNewTextValue =(event) => {
+      this.setState({ newItemText : event.target.value })
+    }
+
+    createNewTodo =() => {
+      if (!this.state.todoItems
+                .find(item => item.action === this.state.newItemText)) {
+                  this.setState({
+                    // spread period
+                    todoItems : [...this.state.todoItems,
+                                  {action: this.state.newItemText,
+                                    done: false}],
+                                    newItemText : ""
+                  });
+                }
     }
 
     changeStateData =() => {
@@ -21,15 +44,24 @@ export default class App extends Component {
       )
     }
 
-
   render =() =>
     <div>
       <h4 className="bg-primary text-white text-center p-2">
-        { this.state.userName } Course
-        <button className="btn btn-danger m-2"
-                onClick={ this.changeStateData }>
-                  Change the State
-        </button>
+        { this.state.userName }'s Todo List
+        ({this.state.todoItems.filter(t=>!t.done).length}) items todo
       </h4>
+
+      <div className="container-fludi">
+        <div className="m-1">
+          <input className="form-control"
+                  value = {this.state.newItemText}
+                  onChange={this.updateNewTextValue} />
+
+                  <button className="btn btn-danger mt-1"
+                          onClick={this.createNewTodo}>
+                            Add New Task
+                  </button>
+        </div>
+      </div>
     </div>
   }
