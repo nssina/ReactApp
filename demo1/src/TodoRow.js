6b1c67be8b180e841535208a83b74bc0738c71d2
nssina <rabie_sina@yahoo.com>
2020-05-27 11:25:09 -0400

    2nd child file added

---
+++ demo1/src/TodoRow.js
@@ -0,0 +1,15 @@
+// 2nd Child Component
+// Do only one Feature
+
+import React, {Component} from 'react'
+
+export class TodoRow extends Component {
+    render =() =>
+    <tr>
+        <td> {this.props.item.action} </td>
+        <td>
+            <input type="checkbox" checked={this.props.item.done}
+                    onChange={()=> this.props.callback(this.props.item)} />
+        </td>
+    </tr>
+}
