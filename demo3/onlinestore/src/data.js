ae6616293c4b01740b9e87f0247065ba96f6c34b
nssina <rabie_sina@yahoo.com>
2020-05-30 15:54:26 -0400

    Started new project for online store

---
+++ demo3/onlinestore/src/data.js
@@ -0,0 +1,14 @@
+module.exports = function() {
+    return {
+        categories: ["waterSports", "soccer", "chess"],
+        products: [
+            {id:1, name: "ABC", category:"waterSports", description: "ABC ED ABC", price: 40},
+            {id:1, name: "ABC", category:"soccer", description: "ABC ED ABC", price: 30},
+            {id:1, name: "ABC", category:"chess", description: "ABC ED ABC", price: 140},
+            {id:1, name: "ABC", category:"waterSports", description: "ABC ED ABC", price: 240},
+            {id:1, name: "ABC", category:"soccer", description: "ABC ED ABC", price: 340},
+            {id:1, name: "ABC", category:"chess", description: "ABC ED ABC", price: 140}
+        ],
+        orders: []
+    }
+}
